fd3828d2a4cd2d72f136b04341478d207c5b3bda
valdir <igeracao@hotmail.com>
2026-06-09 16:10:16 -0300

    feat: configuracao docker e mysql local para testes

--- pro_newmedia/settings.py
+++ pro_newmedia/settings.py
@@ -148,8 +148,16 @@ WSGI_APPLICATION = "pro_newmedia.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": config("MYSQL_DATABASE", default="newmedia_db"),
+        "USER": config("MYSQL_USER", default="newmedia"),
+        "PASSWORD": config("MYSQL_PASSWORD", default="newmediapass"),
+        "HOST": config("MYSQL_HOST", default="db"),
+        "PORT": config("MYSQL_PORT", default="3306"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
